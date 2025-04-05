11f67c62270bc353d2dd1744f892d617f23d8572
Ayush Mohod <ayushmohod36@gmail.com>
2025-04-05 15:36:00 +0530

    Implemented complete and Exit button

--- gui.py
+++ gui.py
@@ -7,10 +7,15 @@ input_box = sg.InputText(tooltip="enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=function.get_todos(), key='todos', enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My TO-DO App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
-                   font=("Helvetica", 10),)
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
+                   font=("Helvetica", 10), )
 
 while True:
     event, values = window.read()
@@ -25,7 +30,7 @@ while True:
             window['todos'].update(values=todos)
         case "Edit":
             todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            new_todo = values['todo'] + "\n"
             todos = function.get_todos()
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
@@ -33,9 +38,15 @@ while True:
             window['todos'].update(values=todos)
         case 'todos':
             window['todo'].update(value=values['todos'][0])
-
-
+        case "Complete":
+            todo_to_complete = values['todo']
+            todos = function.get_todos()
+            todos.remove(todo_to_complete)
+            function.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
         case sg.WIN_CLOSED:
             break
-
+        case "Exit":
+            break
 window.close()
